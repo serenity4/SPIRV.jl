b7ac36b5d895bae58fa6c3404628de4b46342599
serenity4 <cedric.bel@hotmail.fr>
2020-11-28 17:50:24 +0100

    Improve spacing inside functions

--- gen/formatting.jl
+++ gen/formatting.jl
@@ -1,19 +1,26 @@
 function align_block(file; delim=r"=", block_begin=r"@c?enum", block_end=r"^end$")
     lines = readlines(file)
     final_lines = String[]
+
     is_target_block = false
     block_lines = nothing
+
     for (i, l) ∈ enumerate(lines)
+
         if is_target_block
             if !isnothing(match(block_end, l))
                 lengths = map(block_lines) do decl
                     first(findfirst(delim, decl)) - 1
                 end
+
                 nmax = maximum(lengths)
                 j = nmax == first(lengths) ? 2 : 1
+
                 block_lines[j] = rpad(first(block_lines[j], lengths[j]), nmax) * block_lines[j][lengths[j]+1:end]
+
                 append!(final_lines, block_lines)
                 push!(final_lines, l)
+
                 is_target_block = false
             else
                 push!(block_lines, l)
@@ -21,6 +28,7 @@ function align_block(file; delim=r"=", block_begin=r"@c?enum", block_end=r"^end$
         else
             push!(final_lines, l)
         end
+
         if !isnothing(match(block_begin, l))
             block_lines = String[]
             is_target_block = true
@@ -34,17 +42,7 @@ function align_block(file; delim=r"=", block_begin=r"@c?enum", block_end=r"^end$
     format_file(file, margin=92, align_pair_arrow=true, align_assignment=true, align_struct_field=true, align_conditional=true)
 end
 
-function custom_print(io::IO, expr::Expr)
-    if expr.head == :struct
-        println(io)
-        str = string(prettify(expr))
-        str_nonewline = replace(str, "\n" => " ")
-        println(io, str_nonewline)
-        println(io)
-    else
-        println(io, prettify(expr))
-    end
-end
+custom_print(io::IO, expr::Expr) = println(io, prettify(expr))
 
 function format()
     inst_file = src_dir("generated", "instructions.jl")
@@ -52,12 +50,15 @@ function format()
 
     @info "Formatting."
 
+    # initial formatting
     format_file(inst_file, margin=92)
+    format_file(enum_file, margin=92)
+
+    # align blocks
     align_block(inst_file)
     align_block(inst_file, block_begin=r"const class_printing = Dict\(", block_end=r"^\)$", delim=r"=>")
     align_block(inst_file, block_begin=r"const kind_to_category = Dict\(", block_end=r"^\)$", delim=r"=>")
 
-    format_file(enum_file, margin=92)
     align_block(enum_file)
 
     true
--- gen/grammar.jl
+++ gen/grammar.jl
@@ -2,12 +2,8 @@ g = JSON3.read(read(joinpath(include_dir, "spirv.core.grammar.json"), String))
 
 const magic_number = parse(UInt32, g[:magic_number])
 const grammar_version = VersionNumber(getindex.(Ref(g), [:major_version, :minor_version, :revision])...)
-
-instructions = g[:instructions]
-operand_kinds = g[:operand_kinds]
-
-enum_categories = unique(getproperty.(operand_kinds, :category))
-enum_kinds = unique(getproperty.(operand_kinds, :kind))
+const instructions = g[:instructions]
+const operand_kinds = g[:operand_kinds]
 
 function generate_category_as_enum(operand_kinds, category)
     :(@enum $(Symbol(category))::Int begin $([:($(Symbol(kind)) = $val) for (val, kind) ∈ enumerate(getindex.(filter(x -> x["category"] == category, operand_kinds), "kind"))]...) end)
@@ -20,9 +16,11 @@ function generate_operand_enums(value_enum_operands)
         kind = Symbol(op["kind"])
         enum_vals = []
         parameters = []
+
         foreach(op["enumerants"]) do enumerant
             name = enumerant["enumerant"]
             enum_name = Symbol(kind, name)
+
             if hasproperty(enumerant, :parameters)
                 params = map(enumerant[:parameters]) do param
                     # Dict(k => (k == :kind ? (v ∈ getproperty.(op["enumerants"], :enumerant) ? Symbol(kind, v) : Symbol(v)) : strip(v, '\'')) for (k, v) ∈ param)
@@ -30,11 +28,14 @@ function generate_operand_enums(value_enum_operands)
                 end
                 push!(parameters, (enum_name, [:(tuple($([:($k = $v) for (k, v) ∈ param]...))) for param ∈ params]...))
             end
+
             push!(enum_vals, :($enum_name = $(enumerant["value"])))
         end
+
         push!(exprs, :(@cenum $kind::UInt32 begin $(enum_vals...) end))
         !isempty(parameters) && push!(extra_operands, :($kind => Dict($(map(x -> :($(x[1]) => $(x[2])), parameters)...))))
     end
+
     parameter_dict = :(const extra_operands = Dict($(extra_operands...)))
     push!(exprs, parameter_dict)
 
@@ -45,6 +46,7 @@ function generate_instruction_printing_class(insts_pc)
     insts_pc_noexclude = insts_pc[2:end]
     classes = getindex.(insts_pc_noexclude, "tag")
     strs = getindex.(insts_pc_noexclude, "heading")
+
     dict_args = vcat(:(Symbol("@exclude") => ""), [:(Symbol($class) => $str) for (class, str) ∈ zip(classes, strs)])
 
     :(const class_printing = Dict($(dict_args...)))
@@ -84,6 +86,7 @@ mkpath(src_dir("generated"))
 function generate()
     @info "Generating files:"
     @info "  - instructions.jl"
+
     open(src_dir("generated", "instructions.jl"), "w+") do io
         pretty_dump(io, generate_instructions(instructions))
         pretty_dump(io, generate_instruction_printing_class(g[:instruction_printing_class]))
@@ -91,6 +94,7 @@ function generate()
     end
 
     @info "  - enums.jl"
+
     open(src_dir("generated", "enums.jl"), "w+") do io
         pretty_dump(io, generate_category_as_enum(operand_kinds, "Id"))
         pretty_dump(io, generate_category_as_enum(operand_kinds, "Literal"))
