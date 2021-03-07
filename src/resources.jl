607bf8f8cb6a1f57fcce7ae5c25a73b1bf4093e4
serenity4 <cedric.bel@hotmail.fr>
2021-03-07 22:53:15 +0100

    Fix bug in resource reflection

--- src/resources.jl
+++ src/resources.jl
@@ -1,2 +1,2 @@
-bindings(v::Variable) = map(last(filter(x -> first(x) == DecorationBinding, v.decorations)))
-descriptor_sets(v::Variable) = map(last(filter(x -> first(x) == DecorationDescriptorSet, v.decorations)))
+bindings(v::Variable)::Vector{UInt32} = map(last, filter(x -> first(x) == DecorationBinding, v.decorations))
+descriptor_sets(v::Variable)::Vector{UInt32} = map(last, filter(x -> first(x) == DecorationDescriptorSet, v.decorations))
