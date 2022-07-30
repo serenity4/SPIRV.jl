e595f73e8ed029c96a8d5b76b6a7edef1b26d364
serenity4 <cedric.bel@hotmail.fr>
2022-07-30 21:26:27 +0200

    Fix nightly change to `jl_ir_flag_inlineable`

--- src/frontend/interpreter.jl
+++ src/frontend/interpreter.jl
@@ -70,11 +70,17 @@ Core.Compiler.get_inference_cache(si::SPIRVInterpreter) = si.local_cache
 Core.Compiler.code_cache(si::SPIRVInterpreter) = WorldView(si.global_cache, si.world)
 Core.Compiler.method_table(si::SPIRVInterpreter) = si.method_table
 
+@static if VERSION < v"1.9.0-DEV.1054"
+  is_inlineable(src) = ccall(:jl_ir_flag_inlineable, Bool, (Any,), src)
+else
+  using Core.Compiler: is_inlineable
+end
+
 function Core.Compiler.inlining_policy(si::SPIRVInterpreter, @nospecialize(src), stmt_flag::UInt8,
   mi::MethodInstance, argtypes::Vector{Any})
   if isa(src, CodeInfo) || isa(src, Vector{UInt8})
     src_inferred = ccall(:jl_ir_flag_inferred, Bool, (Any,), src)
-    src_inlineable = Core.Compiler.is_stmt_inline(stmt_flag) || ccall(:jl_ir_flag_inlineable, Bool, (Any,), src)
+    src_inlineable = Core.Compiler.is_stmt_inline(stmt_flag) || is_inlineable(src)
     return src_inferred && src_inlineable ? src : nothing
   elseif src === nothing && Core.Compiler.is_stmt_inline(stmt_flag)
     # if this statement is forced to be inlined, make an additional effort to find the
