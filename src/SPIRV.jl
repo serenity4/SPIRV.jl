6d5b6ce6478f6c918f77ae0e3479fea561753401
serenity4 <cedric.bel@hotmail.fr>
2022-09-17 18:48:06 +0200

    Use BitMasks.jl

--- src/SPIRV.jl
+++ src/SPIRV.jl
@@ -11,7 +11,7 @@ using ResultTypes: Result
 using AbstractTrees
 using AbstractTrees: parent
 using SwapStreams: SwapStream
-using Bitmasks
+using BitMasks
 using UUIDs: UUID, uuid1
 using SnoopPrecompile
 @reexport using ResultTypes: iserror, unwrap, unwrap_error
