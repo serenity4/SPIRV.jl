810b8acc7a1e1e4a3d964cde81afeea096fdf3a3
serenity4 <cedric.bel@hotmail.fr>
2022-11-02 17:46:02 +0100

    Turn SSAValue into a primitive type

--- src/ssa.jl
+++ src/ssa.jl
@@ -4,25 +4,26 @@ SSA value used in a SPIR-V context.
 Differs from `Core.SSAValue` in that all SPIR-V constructs will
 use this `SSAValue` type to differentiate with Julia SSA values.
 """
-struct SSAValue
-  id::UInt32
-end
+primitive type SSAValue 32 end
+
+SSAValue(id::UInt32) = reinterpret(SSAValue, id)
+SSAValue(id::Integer) = SSAValue(UInt32(id))
 
 Base.isless(val1::SSAValue, val2::SSAValue) = isless(id(val1), id(val2))
 
-Base.convert(::Type{T}, val::SSAValue) where {T<:Integer} = convert(T, val.id)
-Base.convert(::Type{Core.SSAValue}, val::SSAValue) = Core.SSAValue(val.id)
-Base.convert(::Type{SSAValue}, val::Core.SSAValue) = SSAValue(val.id)
+Base.convert(::Type{T}, val::SSAValue) where {T<:Integer} = convert(T, id(val))
+Base.convert(::Type{Core.SSAValue}, val::SSAValue) = Core.SSAValue(id(val))
+Base.convert(::Type{SSAValue}, val::Core.SSAValue) = SSAValue(id(val))
 Base.convert(::Type{SSAValue}, id::Integer) = SSAValue(id)
 
 Base.zero(::Type{SSAValue}) = SSAValue(zero(UInt32))
 
-id(val::SSAValue) = val.id
+id(val::SSAValue) = reinterpret(UInt32, val)
 
 Base.tryparse(::Type{SSAValue}, id::AbstractString) = !isempty(id) && return SSAValue(parse(UInt32, id[2:end]))
 Base.parse(::Type{SSAValue}, id::AbstractString) = tryparse(SSAValue, id)::SSAValue
 
-Base.show(io::IO, val::SSAValue) = print(io, string('%', val.id))
+Base.show(io::IO, val::SSAValue) = print(io, string('%', id(val)))
 
 const SSADict{T} = Dictionary{SSAValue,T}
 
