ec015ca73a780fa5298c177701e2076a6071090b
serenity4 <cedric.bel@hotmail.fr>
2023-04-14 15:31:10 +0200

    Update precompile traces

--- src/precompile.jl
+++ src/precompile.jl
@@ -1,5 +1,4 @@
-# These precompile directives should be regenerated regularly
-# using `/scripts/generate_precompile.sh`.
+# These precompile directives should be regenerated regularly.
 @precompile_all_calls begin
   @compile_traces verbose = false joinpath(@__DIR__, "precompilation_traces.jl")
 end
