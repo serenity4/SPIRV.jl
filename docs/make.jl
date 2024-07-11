b84c2d9a82045907ad0f4c53b9d6cc41277e7fb1
serenity4 <cedric.bel@hotmail.fr>
2024-07-11 17:01:57 +0200

    Minor doc restructurings

--- docs/make.jl
+++ docs/make.jl
@@ -5,23 +5,12 @@ function julia_files(dir)
   sort(filter(endswith(".jl"), files))
 end
 
-function replace_edit(content)
-  haskey(ENV, "JULIA_GITHUB_ACTIONS_CI") && return content
-  # Linking does not work locally, but we can make
-  # the warning go away with a hard link to the repo.
-  replace(
-      content,
-      r"EditURL = \".*<unknown>/(.*)\"" => s"EditURL = \"https://github.com/JuliaGPU/SPIRV.jl/tree/main/\1\"",
-  )
-end
-
 function generate_markdown_files()
   dir = joinpath(@__DIR__, "src")
   Threads.@threads for file in julia_files(dir)
       Literate.markdown(
           file,
           dirname(file);
-          postprocess = replace_edit,
           documenter = true,
       )
   end
@@ -33,18 +22,20 @@ makedocs(;
   modules = [SPIRV],
   format = Documenter.HTML(prettyurls = true),
   pages = [
-    "Home" => "index.md",
-    "Introduction" => "intro.md",
+    "index.md",
+    "intro.md",
     "Tutorials" => [
       "tutorials/manipulating_spirv.md",
     ],
     "Reference" => [
       "reference/bit_width.md",
     ],
-    "Targeting SPIR-V via LLVM" => "spirv_llvm.md",
-    "Features" => "features.md",
-    "API" => "api.md",
-    "Developer documentation" => "devdocs.md",
+    "Miscellaneous" => [
+      "misc/spirv_llvm.md",
+    ],
+    "features.md",
+    "api.md",
+    "devdocs.md",
   ],
   repo = "https://github.com/serenity4/SPIRV.jl/blob/{commit}{path}#L{line}",
   sitename = "SPIRV.jl",
@@ -56,5 +47,5 @@ makedocs(;
 )
 
 deploydocs(
-  repo = "github.com/serenity4/SPIRV.jl.git",
+  repo = Documenter.Remotes.GitHub("serenity4", "SPIRV.jl"),
 )
--- docs/src/index.md
+++ docs/src/index.md
@@ -1,6 +1,6 @@
 # SPIRV.jl
 
-Tooling around the manipulation of the binary format [SPIR-V](https://www.khronos.org/spir/) defined by the [Khronos Group](https://www.khronos.org/).
+Tooling around the manipulation of the [SPIR-V](https://www.khronos.org/spir/) binary format defined by the [Khronos Group](https://www.khronos.org/).
 
 # Status
 
