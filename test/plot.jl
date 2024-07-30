bcb128ea3e931d1b4bf38fcab3db2c119c8dbbc3
serenity4 <cedric.bel@hotmail.fr>
2024-07-30 15:50:16 +0200

    Improve plot styling for debugging

--- test/plot.jl
+++ test/plot.jl
@@ -4,12 +4,26 @@ using Graphs: Graphs, SimpleDiGraph
 using Plots: plot
 import GraphRecipes
 
-plotcfg(g; names = Graphs.vertices(g), nodesize = 0.3, size = (1000, 1000), kwargs...) = plot(g; names, nodesize, size, kwargs...)
+function edgecolors(cfg::ControlFlowGraph)
+  colors = Dict{Tuple{Int, Int},Any}()
+  insert!(dict, key, value) = haskey(dict, key) ? error("Key already exists: $key") : (dict[key] = value)
+  classification = cfg.ec
+  for edge in edges(cfg)
+    key = (src(edge), dst(edge))
+    in(edge, classification.retreating_edges) && insert!(colors, key, :green)
+    in(edge, classification.tree_edges) && insert!(colors, key, :gray)
+    in(edge, classification.forward_edges) && insert!(colors, key, "#55aaaa")
+    in(edge, classification.cross_edges) && insert!(colors, key, :red)
+    haskey(colors, key) || error("Unclassified edge: $edge")
+  end
+  colors
+end
+
+plotcfg(g; names = Graphs.vertices(g), nodesize = 0.3, size = (1000, 1000), nodeshape = :circle, nodecolor = "#ffbb00", background_color = "rgb(10, 10, 12)", edgecolor=:gray, kwargs...) = plot(g; names, nodesize, size, nodeshape, nodecolor, background_color, edgecolor, kwargs...)
 plotcfg(g::DeltaGraph; kwargs...) = plotcfg(SimpleDiGraph(g); kwargs...)
 plotcfg(tgt::SPIRV.SPIRVTarget; kwargs...) = plotcfg(tgt.cfg; kwargs...)
-plotcfg(cfg::ControlFlowGraph; kwargs...) = plotcfg(cfg.g; kwargs...)
-plotcfg(shader::SPIRV.Shader, args...; kwargs...) = plotcfg(SPIRV.Module(shader), args...; kwargs...)
-plotcfg(mod::SPIRV.Module, args...; kwargs...) = plotcfg(IR(mod), args...; kwargs...)
+plotcfg(cfg::ControlFlowGraph; kwargs...) = plotcfg(cfg.g; edgecolor=edgecolors(cfg), kwargs...)
+plotcfg(mod::SPIRV.Module; kwargs...) = plotcfg(IR(mod); kwargs...)
 plotcfg(ir::IR; kwargs...) = plotcfg(only(ir); kwargs...)
 plotcfg(shader::SPIRV.Shader; kwargs...) = plotcfg(shader.ir; kwargs...)
 function plotcfg(fdef::SPIRV.FunctionDefinition; ssa_indices = true, kwargs...)
